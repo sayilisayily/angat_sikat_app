386d0ac263aabfc04468daa4d0f1967b90080760
Zylei Sugue <zyleisugue@gmail.com>
2024-11-27 04:28:19 +0800

    moe link

--- src/sidebar.php
+++ src/sidebar.php
@@ -32,7 +32,7 @@
                             <a href="../budget_management/budget_overview.php">› Overview </a>
                             <a href="../budget_management/financial_plan.php">› Plan </a>
                             <a href="../budget_management/purchases.php">› Purchases</a>
-                            <a href="#moe">› MOE</a>
+                            <a href="../budget_management/moe.php">› MOE</a>
                             <a href="../budget_management/budget_approval_table.php">› Approval</a>
                         </div>
                         </li>
