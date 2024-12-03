5b10bf84b3c98c796019ced912d70ad8e56ce4fb
Zylei Sugue <zyleisugue@gmail.com>
2024-12-03 16:04:02 +0800

    new db

---
+++ angat_sikat_db.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 03, 2024 at 08:59 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `angat_sikat_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget_allocation`
+--
+-- Error reading structure for table angat_sikat_db.budget_allocation: #1932 - Table 'angat_sikat_db.budget_allocation' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.budget_allocation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`budget_allocation`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget_approvals`
+--
+-- Error reading structure for table angat_sikat_db.budget_approvals: #1932 - Table 'angat_sikat_db.budget_approvals' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.budget_approvals: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`budget_approvals`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table angat_sikat_db.categories: #1932 - Table 'angat_sikat_db.categories' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+-- Error reading structure for table angat_sikat_db.events: #1932 - Table 'angat_sikat_db.events' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.events: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`events`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events_summary`
+--
+-- Error reading structure for table angat_sikat_db.events_summary: #1932 - Table 'angat_sikat_db.events_summary' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.events_summary: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`events_summary`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_items`
+--
+-- Error reading structure for table angat_sikat_db.event_items: #1932 - Table 'angat_sikat_db.event_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.event_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`event_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_summary_items`
+--
+-- Error reading structure for table angat_sikat_db.event_summary_items: #1932 - Table 'angat_sikat_db.event_summary_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.event_summary_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`event_summary_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+-- Error reading structure for table angat_sikat_db.expenses: #1932 - Table 'angat_sikat_db.expenses' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.expenses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`expenses`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `financial_plan`
+--
+-- Error reading structure for table angat_sikat_db.financial_plan: #1932 - Table 'angat_sikat_db.financial_plan' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.financial_plan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`financial_plan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance`
+--
+-- Error reading structure for table angat_sikat_db.maintenance: #1932 - Table 'angat_sikat_db.maintenance' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.maintenance: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`maintenance`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_items`
+--
+-- Error reading structure for table angat_sikat_db.maintenance_items: #1932 - Table 'angat_sikat_db.maintenance_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.maintenance_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`maintenance_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_summary`
+--
+-- Error reading structure for table angat_sikat_db.maintenance_summary: #1932 - Table 'angat_sikat_db.maintenance_summary' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.maintenance_summary: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`maintenance_summary`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_summary_items`
+--
+-- Error reading structure for table angat_sikat_db.maintenance_summary_items: #1932 - Table 'angat_sikat_db.maintenance_summary_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.maintenance_summary_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`maintenance_summary_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organizations`
+--
+-- Error reading structure for table angat_sikat_db.organizations: #1932 - Table 'angat_sikat_db.organizations' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.organizations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`organizations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+-- Error reading structure for table angat_sikat_db.purchases: #1932 - Table 'angat_sikat_db.purchases' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.purchases: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`purchases`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases_summary`
+--
+-- Error reading structure for table angat_sikat_db.purchases_summary: #1932 - Table 'angat_sikat_db.purchases_summary' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.purchases_summary: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`purchases_summary`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_items`
+--
+-- Error reading structure for table angat_sikat_db.purchase_items: #1932 - Table 'angat_sikat_db.purchase_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.purchase_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`purchase_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_summary_items`
+--
+-- Error reading structure for table angat_sikat_db.purchase_summary_items: #1932 - Table 'angat_sikat_db.purchase_summary_items' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.purchase_summary_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`purchase_summary_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table angat_sikat_db.users: #1932 - Table 'angat_sikat_db.users' doesn't exist in engine
+-- Error reading data for table angat_sikat_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `angat_sikat_db`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
